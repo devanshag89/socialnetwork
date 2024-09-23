68c1e67c7a1327cb8aedf92b9cf4dd317c294247
Devansh Agrawal <122165428+devanshcs22@users.noreply.github.com>
2024-09-23 20:10:59 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,49 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userModel = require('./models/user');
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/user");
+
 const PORT = 3000;
 
-// Middleware to parse JSON data
-app.use(express.json());
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    userModel.findOne({email: email})
+    .then(user => {
+        if(user){
+            if(user.password === password){
+                res.json("success")
+            }else{
+                res.json("password is incorrect")
+            }
+        }else{
+            res.json("user does not exist")
+        }
+    })
+})
 
-// A simple GET route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
 
-// A POST route
-app.post('/data', (req, res) => {
-    const data = req.body;
-    res.json({
-        message: 'Data received successfully',
-        receivedData: data
-    });
-});
+app.post('/register', (req, res) => {
+    // userModel.create(req.body)
+    // .then(userRegister => res.json(userRegister))
+    // .catch(err => res.json(err))
+    
+    const userdata = req.body;
+    userModel.findOne({email: userdata.email})
+    .then(user => {
+        if(user){
+            res.json("user already exists")
+        }else{
+            userModel.create(userdata);
+            res.json(user)
+        }
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
