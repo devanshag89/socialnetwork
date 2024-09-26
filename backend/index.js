b746a0ded5ef0c497cc5899cab0d4112844781ba
Devansh Agrawal <122165428+devanshcs22@users.noreply.github.com>
2024-09-26 19:35:46 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const userModel = require('./models/user');
 
 const app = express();
 app.use(express.json());
+
 app.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/user");
